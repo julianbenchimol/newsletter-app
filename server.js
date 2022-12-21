dd82ef3c92068d69f39dd7dddc977743bbfd3112
David Parker <david.m.parker92@gmail.com>
2022-12-21 15:32:13 -0500

    added some server.js

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+
+// Import model to sync table with database
+const emails = require('./Models/emails');
+const newsLetter = require('./Models/newsLetter');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // for model
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
