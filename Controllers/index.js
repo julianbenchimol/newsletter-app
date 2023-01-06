78c5344c017b7fa9cab629b708caa091522466f4
JulianBenchimol <jbenchimol1999@gmail.com>
2023-01-05 20:13:22 -0500

    worked on seeding the database; created login.js

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const userRoutes = require('./api/user-routes.js')
+const userRoutes = require('./user-routes.js')
 const apiRoutes = require('./api')
 
 router.use('/', userRoutes);
--- Models/user.js
+++ Models/user.js
@@ -1,5 +1,5 @@
+const sequelize = require('../Config/connection')
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../Config/connection');
 const bycrypt = require('bcrypt');
 
 class User extends Model {
