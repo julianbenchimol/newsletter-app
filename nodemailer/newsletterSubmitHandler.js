32f69f38a0b2faa2e03116e0b29daa9e357a3dab
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-01-08 16:22:52 -0500

    modified newsletterSubmitHandler and emailForm.handlebars

--- nodemailer/newsletterSubmitHandler.js
+++ nodemailer/newsletterSubmitHandler.js
@@ -1,22 +1,44 @@
-const emailTo = document.getElementById("emailTo").val().trim();
-const emailFrom = document.getElementById("emailFrom").val().trim();
-const subject = document.getElementById("subject").val().trim();
-const title = document.getElementById("title").val().trim();
-const message = document.getElementById("messageText").val().trim();
-const imageSrc = document.getElementById("imageSrc");
+const emailTo = document.getElementById("emailTo").val().trim(); //require the array of recipients
+const emailFrom = document.getElementById("emailFrom").val().trim(); //user logged in
+const subject = document.getElementById("subject").val().trim(); //user input
+const title = document.getElementById("title").val().trim(); //user input
+const message = document.getElementById("messageText").val().trim(); //user input
+const imageSrc = document.getElementById("imageSrc"); //user input
 const submitBtn = document.getElementById("submitForm");
 
 function writeNewsletter() {
 
-   const newsletterHTML = `<div><img src="${imageSrc}">
-   \n<p>To: ${emailTo}</p>
-   \n<p>From: ${emailFrom}</p>
-   \n<p>Subject: ${subject}</p>
-    </div>
-    \n
-    \n<div><h2>${title}</h2>
-    \n<p>${message}</p>   
-    </div>`;
+   const newsletterHTML = 
+   `<!DOCTYPE html>
+   <html lang="en">
+     <head>
+       <meta charset="UTF-8" />
+       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+       <link rel="stylesheet" href="./assets/style.css"/>>
+       <title>Email Newsletter</title>
+     </head>
+     <body>
+       <header id="top">
+       
+       </header>
+       <main>
+         <!-- Student code goes here -->
+         <div class="d-flex" style="flex-direction: column;"><img src="${imageSrc}" class="d-flex">
+        \n<p>To: ${emailTo}</p>
+        \n<p>From: ${emailFrom}</p>
+        \n<p>Subject: ${subject}</p></div>
+       \n
+       \n<div><h2>${title}</h2>
+       \n<p>${message}</p>   
+       </div>
+       </main>
+       <footer>
+         
+       </footer>
+       <script src="./assets/script.js"></script>
+     </body>
+   </html>`;
 
     console.log(emailTo, /*The email from field should be the email of the user logged in */, subject, title, message, newsletterHTML);
 
