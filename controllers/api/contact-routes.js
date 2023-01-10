d7f812ee48844ddc030d017d214f7095c9224918
JulianBenchimol <jbenchimol1999@gmail.com>
2023-01-10 11:48:52 -0500

    created contact route; needs hbs connection + button

--- controllers/api/contact-routes.js
+++ controllers/api/contact-routes.js
@@ -4,7 +4,7 @@ const withAuth = require('../../utils/auth');
 
 router.get('/', withAuth, async(req, res)=>{
     try{ 
-        res.render('newsletter')
+        res.render('')
     }
     catch(err){
         req.statusCode(500).json(err)
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const newsletterRoutes = require('./newsletter')
+const contactRoutes = require('./contact-routes')
 
 router.use('/users', userRoutes);
 router.use('/newsletter', newsletterRoutes)
